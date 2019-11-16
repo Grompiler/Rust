5eaa40dbfb1ba9df00db0255e27a841869001f86
Pierre Leblond <32479816+Pioterr@users.noreply.github.com>
2019-11-16 16:43:34 +0100

    Update main.rs

--- rpg/src/main.rs
+++ rpg/src/main.rs
@@ -1,25 +1,27 @@
-mod hero;
-mod weapon;
+// on declare via le main tous les modules du projet qu'on
+// veut utiliser dans le reste de la crate (=le reste des XXX.rs)
+mod hero; // "bring files hero.rs & weapon.rs to the scope"
+mod weapon; // pour pouvoir utiliser "use crate::weapon::*;" dans hero.rs
+mod action;
 
+
+// on declare simplement ce qu'on utilise directement dans le main
 use hero::*;
-use weapon::*;
 
 
 fn main() {
 
-    let first_weapon = Weapon::new(50, WeaponType::Sword);
-
-    let mut first_hero = Hero::new(200, HeroType::Knight, first_weapon);
+ 
+    let mut first_hero = Hero::new(200, &HeroType::Knight, Position{x:0, y:0});
     first_hero.info();
 
-    let second_weapon = Weapon::new(50, WeaponType::Sword);
-    let mut second_hero = Hero::new(210, HeroType::Knight, second_weapon);
+    let mut second_hero = Hero::new(210, &HeroType::Knight, Position{x:10, y:10});
     second_hero.info();
 
     
     while first_hero.is_alive() && second_hero.is_alive() {
         first_hero.attack(&mut second_hero);
-        second_hero.attack(&mut first_hero);
+        if second_hero.is_alive(){second_hero.attack(&mut first_hero);}
 
         first_hero.info();
         second_hero.info();
