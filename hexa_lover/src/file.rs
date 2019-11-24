e4678a4e5725f11a31ef508fe6bf2c08a907b3f7
Pierre Leblond <32479816+Pioterr@users.noreply.github.com>
2019-11-24 13:52:02 +0100

    Add files via upload

---
+++ hexa_lover/src/file.rs
@@ -0,0 +1,18 @@
+
+// use std::io;
+// use std::io::prelude::*;
+// use std::fs::File;
+
+
+
+
+// pub fn read_binary(input_filename: &str) -> io::Result<Vec<u8>>{
+
+//     let mut f = File::open(input_filename)?;
+//     let mut buffer = Vec::new();
+//     // read the whole file
+//     f.read_to_end(&mut buffer)?;
+//     // println!("{:?}", buffer);
+
+//     Ok(buffer)
+// }
